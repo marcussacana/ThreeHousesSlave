09abf131198431bee71677ee351127a31025aade
Marcus André <ddtank.marcus@gmail.com>
2023-03-16 05:40:17 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,197 @@
+﻿using Microsoft.VisualBasic;
+using ThreeHousesSlave;
+
+Console.Title = "Tree Houses Translation Slave Tool";
+if (args == null || args.Length != 1)
+{
+    Console.WriteLine("Drag&Drop a extracted DATA0/DATA1 Directory to this tool executable");
+    Console.WriteLine("Press a key to exit");
+    Console.ReadKey();
+    return;
+}
+
+var Dir = args[0];
+
+if (!Directory.Exists(Dir))
+{
+    Console.WriteLine("Invalid Directory");
+    return;
+}
+
+var ExtractFileID = (string Name) =>
+{
+    var FName = Path.GetFileNameWithoutExtension(Name);
+    string ID = string.Empty;
+    foreach (var Char in FName)
+    {
+        if (char.IsNumber(Char))
+            ID += Char;
+        else
+            break;
+    }
+
+    return int.Parse(ID);
+};
+
+var Files = Directory.GetFiles(Dir, "*.bin");
+var TxtFiles = Directory.GetFiles(Dir, "*.txt");
+
+if (!Files.Any(x => x.EndsWith("_str.bin")) && !Files.Any(x => x.EndsWith("_scene.bin")))
+{
+    Console.WriteLine("Running Text Detection...");
+    foreach (var FilePath in Files.OrderBy(x => ExtractFileID(x)))
+    {
+        Console.WriteLine($"Checking {Path.GetFileName(FilePath)}...");
+
+        string NewName = null;
+        using (var Stream = File.OpenRead(FilePath))
+        {
+            if (TextSFile.IsValid(Stream))
+            {
+                NewName = $"{ExtractFileID(FilePath)}_str.bin";
+            }
+
+            if (SceneText.IsValid(Stream))
+            {
+                NewName = $"{ExtractFileID(FilePath)}_scene.bin";
+            }
+        }
+
+        if (NewName == null)
+            continue;
+
+
+        var NewFilePath = Path.Combine(Path.GetDirectoryName(FilePath) ?? throw new NullReferenceException(nameof(FilePath)), NewName);
+        File.Move(FilePath, NewFilePath);
+
+        Console.WriteLine("New File name: " + Path.GetFileName(NewFilePath));
+    }
+    return;
+}
+
+var Escaper = (string String, bool Enable) =>
+{
+    if (Enable)
+    {
+        string Result = string.Empty;
+        foreach (char c in String)
+        {
+            if (c == '\n')
+                Result += "\\n";
+            else if (c == '\\')
+                Result += "\\\\";
+            else if (c == '\t')
+                Result += "\\t";
+            else if (c == '\r')
+                Result += "\\r";
+            else
+                Result += c;
+        }
+        String = Result;
+    }
+    else
+    {
+        string Result = string.Empty;
+        bool Special = false;
+        foreach (char c in String)
+        {
+            if (c == '\\' & !Special)
+            {
+                Special = true;
+                continue;
+            }
+            if (Special)
+            {
+                switch (c.ToString().ToLower()[0])
+                {
+                    case '\\':
+                        Result += '\\';
+                        break;
+                    case 'n':
+                        Result += '\n';
+                        break;
+                    case 't':
+                        Result += '\t';
+                        break;
+                    case 'r':
+                        Result += '\r';
+                        break;
+                    default:
+                        throw new Exception("\\" + c + " Isn't a valid string escape.");
+                }
+                Special = false;
+            }
+            else
+                Result += c;
+        }
+        String = Result;
+    }
+
+    return String;
+};
+
+
+if (!TxtFiles.Any(x => x.EndsWith("_str.txt")) && !TxtFiles.Any(x => x.EndsWith("_scene.txt")))
+{
+    Console.WriteLine("Running Text Extraction...");
+    foreach (var FilePath in Files.Where(x => x.EndsWith("_str.bin")).OrderBy(x => ExtractFileID(x)))
+    {
+        Console.WriteLine($"Dumping {Path.GetFileName(FilePath)}...");
+        string TxtPath = Path.ChangeExtension(FilePath, "txt");
+        var Data = File.ReadAllBytes(FilePath);
+        var Script = new TextSFile(Data);
+        var Lines = Script.Import();
+        var Escaped = Lines.Select(x => Escaper(x, true)).ToArray();
+        File.WriteAllLines(TxtPath, Escaped);
+    }
+
+    foreach (var FilePath in Files.Where(x => x.EndsWith("_scene.bin")).OrderBy(x => ExtractFileID(x)))
+    {
+        Console.WriteLine($"Dumping {Path.GetFileName(FilePath)}...");
+        string TxtPath = Path.ChangeExtension(FilePath, "txt");
+        var Data = File.ReadAllBytes(FilePath);
+        var Script = new SceneText(Data);
+        var Lines = Script.Import();
+        var Escaped = Lines.Select(x => Escaper(x, true)).ToArray();
+        File.WriteAllLines(TxtPath, Escaped);
+    }
+    return;
+}
+
+Console.WriteLine("Running Text Insertion...");
+
+foreach (var FilePath in TxtFiles.Where(x => x.EndsWith("_str.txt")).OrderBy(x => ExtractFileID(x)))
+{
+    Console.WriteLine($"Inserting {Path.GetFileName(FilePath)}...");
+    string BinPath = Path.ChangeExtension(FilePath, "bin");
+    string NewBinPath = BinPath + ".new";
+    var Data = File.ReadAllBytes(BinPath);
+    var Script = new TextSFile(Data);
+    var Lines = Script.Import();
+    var NewText = File.ReadAllLines(FilePath);
+    for (int i = 0; i < Lines.Length; i++)
+    {
+        Lines[i] = Escaper(NewText[i], false);
+    }
+
+    var NewData = Script.Export(Lines);
+    File.WriteAllBytes(NewBinPath, NewData);
+}
+
+foreach (var FilePath in TxtFiles.Where(x => x.EndsWith("_scene.txt")).OrderBy(x => ExtractFileID(x)))
+{
+    Console.WriteLine($"Inserting {Path.GetFileName(FilePath)}...");
+    string BinPath = Path.ChangeExtension(FilePath, "bin");
+    string NewBinPath = BinPath + ".new";
+    var Data = File.ReadAllBytes(BinPath);
+    var Script = new SceneText(Data);
+    var Lines = Script.Import();
+    var NewText = File.ReadAllLines(FilePath);
+    for (int i = 0; i < Lines.Length; i++)
+    {
+        Lines[i] = Escaper(NewText[i], false);
+    }
+
+    var NewData = Script.Export(Lines);
+    File.WriteAllBytes(NewBinPath, NewData);
+}
---
+++ SceneText.cs
@@ -0,0 +1,147 @@
+﻿using AdvancedBinary;
+using System.Text;
+
+namespace ThreeHousesSlave
+{
+    internal class SceneText
+    {
+        struct TextEntry
+        {
+            public uint Offset;
+            public uint Length;
+        };
+
+        struct SCText
+        {
+            public uint Count;
+
+            [RArray(nameof(Count)), StructField]
+            public TextEntry[] Entries;
+        }
+
+        byte[] Script;
+        public SceneText(byte[] Data)
+        {
+            this.Script = Data;
+        }
+
+        public string[] Import()
+        {
+            using (var stream = new MemoryStream(Script))
+            {
+                if (!IsValid(stream))
+                    throw new InvalidDataException();
+
+                stream.Position = 0;
+
+                var Reader = new StructReader(stream);
+                SCText OffsetTable = new SCText();
+                Reader.ReadStruct(ref OffsetTable);
+
+                List<string> Strings = new List<string>();
+                for (int i = 0; i < OffsetTable.Count; i++)
+                {
+                    var TableEntry = OffsetTable.Entries[i];
+                    Reader.Position = TableEntry.Offset;
+
+                    var StrBuffer = new byte[TableEntry.Length + 1];
+                    if (Reader.Read(StrBuffer, 0, StrBuffer.Length) != TableEntry.Length + 1)
+                    {
+                        throw new InvalidDataException();
+                    }
+
+                    Strings.Add(Encoding.UTF8.GetString(StrBuffer));
+                }
+                return Strings.ToArray();
+            }
+        }
+
+        public byte[] Export(string[] Content)
+        {
+            using (var Stream = new MemoryStream())
+            using (var OutStream = new MemoryStream())
+            {
+                var Entries = new TextEntry[Content.Length];
+                var StrWriter = new StructWriter(Stream);
+
+                int BaseOffset = Content.Length * 8 + 4;
+                for (int i = 0; i < Content.Length; i++)
+                {
+                    var StringData = Encoding.UTF8.GetBytes(Content[i] + "\x0\x0");
+                    var Length = (uint)(StringData.Length - 2);
+                    if (StringData.Length % 2 != 0)
+                    {
+                        Array.Resize(ref StringData, StringData.Length + 1);
+                    }
+
+                    Entries[i] = new TextEntry() { 
+                        Offset = (uint)(BaseOffset + Stream.Length),
+                        Length = Length - 1
+                    };
+
+                    StrWriter.Write(StringData);
+                }
+
+                StrWriter.Flush();
+
+                var SCFile = new SCText();
+                SCFile.Count = (uint)Content.Length;
+                SCFile.Entries = Entries;
+
+                StructWriter Writer = new StructWriter(OutStream);
+                Writer.WriteStruct(ref SCFile);
+                Writer.Write(Stream.ToArray());
+
+                Writer.Flush();
+
+                return OutStream.ToArray();
+            }
+        }
+
+        public static bool IsValid(Stream Stream)
+        {
+            try
+            {
+                var Reader = new StructReader(Stream);
+                Reader.Position = 0;
+
+                var Count = Reader.ReadUInt32();
+                var FirstOffset = Reader.ReadUInt32();
+
+                Reader.Position = 0;
+
+                if (Count * 8 + 4 != FirstOffset)
+                    return false;
+
+                SCText OffsetTable = new SCText();
+                Reader.ReadStruct(ref OffsetTable);
+
+                long LastReadEndPos = 0;
+                for (uint i = 0; i < OffsetTable.Count; i++)
+                {
+                    var Offset = OffsetTable.Entries[i].Offset;
+                    var Length = OffsetTable.Entries[i].Length;
+                    if (Offset > Stream.Length || Offset < FirstOffset || (LastReadEndPos != 0 && LastReadEndPos > Offset))
+                        return false;
+
+                    Reader.Position = Offset;
+                    var Bytes = Reader.ReadBytes((int)Length);
+                    LastReadEndPos = Reader.Position;
+
+                    if (Bytes.Any(x=> x < 10))
+                        return false;
+
+                    Reader.Position = Offset - 1;
+                    var Byte = Reader.PeekByte();
+                    if (i > 0 && Byte != 0)
+                        return false;
+                }
+
+                return true;
+            }
+            catch {
+                return false;
+            }
+        }
+    }
+}
